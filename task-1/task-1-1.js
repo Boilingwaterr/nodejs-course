d12adab664caafbbb339d8469812e6607318ca3f
Boilingwaterr <floppyrip@gmail.com>
2021-09-27 01:42:45 +0300

    Delete task-1 directory

--- task-1/task-1-1.js
+++
@@ -1,9 +0,0 @@
-function main() {
-  process.stdin.on("data", (inputStdin) => {
-    const output = inputStdin.toString("utf8").split("").reverse().join("");
-
-    process.stdout.write(output, console.error);
-  });
-}
-
-main();
