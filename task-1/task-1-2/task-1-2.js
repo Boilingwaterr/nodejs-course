f901fce68e48a64711cbdf2cfd157e2e08fb29a4
boilingwaterr <floppyrip@gmail.com>
2021-10-01 02:00:50 +0300

    fix issues

--- task-1/task-1-2/task-1-2.js
+++ task-1/task-1-2/task-1-2.js
@@ -10,7 +10,26 @@ const writeStream = createWriteStream(output);
 writeStream.on("error", handleError);
 readStream.on("error", handleError);
 
-readStream.pipe(csv()).pipe(writeStream);
+readStream
+  .pipe(
+    csv({
+      ignoreColumns: /amount/,
+      colParser: { price: "number" },
+    }).preFileLine((fileLineString, lineIdx) => {
+      return new Promise((resolve) => {
+        let newLine = fileLineString;
+
+        if (lineIdx === 0) {
+          newLine = fileLineString.toLowerCase();
+        }
+
+        resolve(newLine);
+      });
+    })
+  )
+  .on("error", handleError)
+  .pipe(writeStream)
+  .on("error", handleError);
 
 function handleError(error) {
   console.error(error.message);
